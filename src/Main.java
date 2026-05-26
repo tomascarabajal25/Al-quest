e1d74fb38e898996b7a4ad3921ed3d4dac1c73a6
ElianaMassin <emassin@fi.uba.ar>
2026-05-26 02:32:22 -0300

    "refactor(main): delegar creacion de ventana a VentanaPrincipal con VictoriaListener"

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import ciudades.reinas.ui.VentanaPrincipal;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new VentanaPrincipal(() -> {
+
+                // acá se conectará con la siguiente ciudad cuando exista, es un print temporal
+                System.out.println("Ciudad completada");
+
+            });
+        });
+    }
+}
