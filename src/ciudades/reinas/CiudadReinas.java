a1e6d2d22b5a13e758b41903800ce9cf03216b0a
ElianaMassin <emassin@fi.uba.ar>
2026-05-26 10:43:01 -0300

    "fix: correccion de tabulacion de un metodo"

--- src/ciudades/reinas/CiudadReinas.java
+++ src/ciudades/reinas/CiudadReinas.java
@@ -17,30 +17,30 @@ public class CiudadReinas {
     }
 
     public boolean validarTableroJugador(int[][] tableroJugador) {
-    int[] columnas = new int[tablero.getTamanio()];
+        int[] columnas = new int[tablero.getTamanio()];
 
-    // exactamente una reina por fila
-    for (int fila = 0; fila < tablero.getTamanio(); fila++) {
-        int reinasEnFila = 0;
-        for (int col = 0; col < tablero.getTamanio(); col++) {
-            if (tableroJugador[fila][col] == 1) {
-                reinasEnFila++;
-                columnas[fila] = col;
+        // exactamente una reina por fila
+        for (int fila = 0; fila < tablero.getTamanio(); fila++) {
+            int reinasEnFila = 0;
+            for (int col = 0; col < tablero.getTamanio(); col++) {
+                if (tableroJugador[fila][col] == 1) {
+                    reinasEnFila++;
+                    columnas[fila] = col;
+                }
+            }
+            if (reinasEnFila != 1) return false;
+        }
+
+        // ninguna se ataca entre sí
+        for (int i = 0; i < tablero.getTamanio(); i++) {
+            for (int j = i + 1; j < tablero.getTamanio(); j++) {
+                if (columnas[i] == columnas[j]) return false;
+                if (Math.abs(i - j) == Math.abs(columnas[i] - columnas[j])) return false;
             }
         }
-        if (reinasEnFila != 1) return false;
-    }
 
-    // ninguna se ataca entre sí
-    for (int i = 0; i < tablero.getTamanio(); i++) {
-        for (int j = i + 1; j < tablero.getTamanio(); j++) {
-            if (columnas[i] == columnas[j]) return false;
-            if (Math.abs(i - j) == Math.abs(columnas[i] - columnas[j])) return false;
-        }
+        return true;
     }
-
-    return true;
-}
 
     public void actualizarTableroJugador(int[][] tableroJugador, int filaInicial, int colInicial) {
         tablero.setTamanio(tablero.getTamanio());
