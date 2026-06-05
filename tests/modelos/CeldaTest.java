e4f58891a5a5c0f16ba73e435b2cc6cd518a46f5
Tomas Carabajal <tcarabajal@fi.uba.ar>
2026-06-05 11:10:09 -0300

    feat: TDA Test del modelo Partida

--- tests/modelos/CeldaTest.java
+++ tests/modelos/CeldaTest.java
@@ -1,9 +1,7 @@
-package tests.modelos;
+package modelos;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import modelos.Celda;
 
 import static org.junit.jupiter.api.Assertions.*;
 
