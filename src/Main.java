264a061839a5c8bba26b11f35f3a2517a4db55d1
lautsoria <ltsoria@fi.uba.ar>
2026-06-14 01:31:46 -0300

    fix/solucionar problema de rayas al mover el mouse

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import persistencia.GestorDeInicio;
 
 public class Main {
     public static void main(String[] args) {
+    	System.setProperty("sun.java2d.d3d", "false");
         PartidaGeneral partidaGeneral = GestorDeInicio.iniciarSesion();
 
         if (partidaGeneral == null) {
